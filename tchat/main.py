8eef10d38970c3c5f4730b81d753ca36550d7212
musamauet <usamarabani@renesistech.com>
2024-07-18 17:58:35 +0500

    implement ConversationBufferMemory and ConversationSummaryMemory

--- tchat/main.py
+++ tchat/main.py
@@ -1,7 +1,8 @@
 from langchain import LLMChain
 # from langchain.chat_models import ChatOpenAI
 from langchain_openai import ChatOpenAI
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory, ConversationSummaryMemory
 from dotenv import load_dotenv
 import os
 
@@ -12,21 +13,32 @@ print(f'key Muhammad {api_key}')
 load_dotenv()
 
 chat = ChatOpenAI(openai_api_key=api_key)
-
+# memory = ConversationBufferMemory(
+#     chat_memory=FileChatMessageHistory("messages.json"),
+#     memory_key="messages",
+#     return_messages=True
+#     )
+memory = ConversationSummaryMemory(
+    memory_key="messages",
+    return_messages=True,
+    llm=chat
+)
 prompt = ChatPromptTemplate(
     input_variables=['content'],
     messages=[
+        MessagesPlaceholder(variable_name="messages"),
         HumanMessagePromptTemplate.from_template("{content}")
     ]
 )
 
 chain = LLMChain(
     llm=chat,
-    prompt=prompt
+    prompt=prompt,
+    memory=memory
 )
 
 while True:
-    content = print(">> ")
+    content = input(">> ")
     
     result = chain({'content': content})
     print(result['text'])
